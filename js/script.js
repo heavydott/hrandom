5c7a0c8d5411e5a0bb8e6e8b14ac14580f5156db
heavydott <shustart@gmail.com>
2017-03-07 14:10:15 +0300

    number bug fix

--- js/script.js
+++ js/script.js
@@ -10,15 +10,21 @@ $btn.on('click', function() {
 	var data = $in.val();
 
 	if(isNumeric(data)) {
-		var random;
-		var $rand;
+		if((data / 10000000000) < 1) {
+			var random;
+			var $rand;
 
-		data = parseInt(data, 10);
+			data = parseInt(data, 10);
 
-		random = Math.floor((Math.random() * data) + 1);
+			random = Math.floor((Math.random() * data) + 1);
 
-		$rand = '<p>' + random + '</p>';
+			$rand = '<p>' + random + '</p>';
 
-		$result.html($rand);
+			$result.html($rand);
+		} else {
+			$result.html('<span>Введите меньшее число</span>');
+		}
+	} else {
+		$result.html('<span>Введите число</span>');
 	}
 });
