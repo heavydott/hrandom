0ba4e630476052a0450ec76b873c5515681ae5b4
heavydott <shustart@gmail.com>
2017-03-07 16:46:44 +0300

    coin was added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
+var $name = $('#name');
+var $containerNumber = $('#container-number');
 var $in = $('#in');
 var $result = $('#result');
 var $btn = $('#btn');
+var $containerCoin = $('#container-coin');
+var $container = $('#container');
+var $coin = $('#coin');
 
+// Check if number
 function isNumeric(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+// Coin / Number
+$name.on('click', function() {
+	if($containerNumber.css('display') == 'block') {
+		$containerNumber.css('display', 'none');
+		$containerCoin.css('display', 'block');
+		$name.html('NUMBER');
+	} else {
+		$containerNumber.css('display', 'block');
+		$containerCoin.css('display', 'none');
+		$name.html('COIN');
+	}
+});
+
+// Random button event
 $btn.on('click', function() {
 	var data = $in.val();
 
@@ -22,9 +42,55 @@ $btn.on('click', function() {
 
 			$result.html($rand);
 		} else {
-			$result.html('<span>Введите меньшее число</span>');
+			$result.html('<span>Enter a smaller number</span>');
 		}
 	} else {
-		$result.html('<span>Введите число</span>');
+		$result.html('<span>Enter a number</span>');
 	}
 });
+
+// Heads or tails handler
+function headsOrTails() {
+	var flip, flipBack;
+	var randCoin = Math.floor((Math.random() * 10) + 1);
+
+	$coin.off('click');
+
+	flip = setInterval(function() {
+		$coin.addClass('flip');
+	}, 100);
+
+	setTimeout(function() {
+		flipBack = setInterval(function() {
+			$coin.removeClass('flip');
+		}, 150);
+	}, 100);
+
+	$container.addClass('bigger');
+
+	setTimeout(function() {
+		$container.removeClass('bigger');
+	}, 1500);
+
+	setTimeout(function() {
+		clearInterval(flip);
+		clearInterval(flipBack);
+
+		if(randCoin > 5) {
+			if(!$coin.hasClass('flip')) {
+				$coin.addClass('flip');
+			}
+		} else {
+			if($coin.hasClass('flip')) {
+				$coin.removeClass('flip');
+			}
+		}
+	}, 2000);
+
+	setTimeout(function() {
+		$coin.on('click', headsOrTails);
+	}, 2500);
+}
+
+// Heads or tails event
+$coin.on('click', headsOrTails);
